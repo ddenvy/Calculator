ba8d479aafc4dbee95081f4936351aa4eb5cde8e
ddenvy <envydany@hotmail.com>
2023-02-04 15:29:38 +0800

    calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        string c = Console.ReadLine();
+        int b = Convert.ToInt32(Console.ReadLine());
+
+
+        int d;
+
+
+        switch (c)
+        {
+            case "+":
+                d = a + b;
+                Console.WriteLine(d);
+                break;
+            case "-":
+                d = a - b;
+                Console.WriteLine(d);
+                break;
+            case "*":
+                d = a * b;
+                Console.WriteLine(d);
+                break;
+            case "/":
+                d = a / b;
+                Console.WriteLine(d);
+                break;
+            default:
+                Console.WriteLine(" ");
+                break;
+
+        }
+    }
+}
